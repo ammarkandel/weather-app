2c3315f1db5e06a670b9ce3b5847722085ec429f
ammarkandel <merom8703@gmail.com>
2021-04-15 00:26:47 +0200

    Add some style

--- src/app/components/searchEvent.js
+++ src/app/components/searchEvent.js
@@ -1,9 +1,10 @@
 import fetchCities from './fetchData';
 
 const submitCityName = document.getElementById('search');
+const cityName = document.querySelector('.city');
 
 const searchEvent = () => {
-  return submitCityName.addEventListener('click', (e) => {
+  submitCityName.addEventListener('click', (e) => {
     e.preventDefault();
     let city = cityName.value;
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3471d2dec0a5bbfa50a6a6a9569cbe0a&units=metric`;
