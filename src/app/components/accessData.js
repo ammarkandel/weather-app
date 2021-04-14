d290815e3e60d7ba6d40623672cedc4cd460ccf1
ammarkandel <merom8703@gmail.com>
2021-04-15 00:33:16 +0200

    Format date

--- src/app/components/accessData.js
+++ src/app/components/accessData.js
@@ -5,8 +5,8 @@ const weatherDetails = document.querySelector('.weather_details');
 
 const printData = (data) => {
   printCityName.textContent = data.name;
-  const sunrise = moment(new Date(data.sys['sunrise'] * 1000)).calendar();
-  const sunset = moment(new Date(data.sys['sunset'] * 1000)).calendar();
+  const sunrise = moment(new Date(data.sys['sunrise'] * 1000)).format('h:mm:ss a');
+  const sunset = moment(new Date(data.sys['sunset'] * 1000)).format('h:mm:ss a');
   weatherDetails.innerHTML = `
   <li><span>Sun rise</span> ${sunrise}</li>
   <li><span>Sun set</span> ${sunset}</li>
