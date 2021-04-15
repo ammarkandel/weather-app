e7667858f88e41ae45ba5f5f6d4a2e2fa49ed6d3
ammarkandel <merom8703@gmail.com>
2021-04-15 06:33:40 +0200

    Add descriptive readme file

--- dist/main.5df7724cef9efadf803d.js
+++
@@ -1,1 +0,0 @@
-(()=>{"use strict";document.querySelector(".search"),document.querySelector(".city_name");const e=document.querySelector(".city").value;fetchUsers(`https://api.openweathermap.org/data/2.5/weather?q=${e}&appid=3471d2dec0a5bbfa50a6a6a9569cbe0a`)})();
--- src/app/components/accessData.js
+++ src/app/components/accessData.js
@@ -11,7 +11,7 @@ const printData = (data) => {
   <li><span>Sun rise</span> ${sunrise}</li>
   <li><span>Sun set</span> ${sunset}</li>
   <li><span>Wind speed</span> ${data.wind.speed} meters/s</li>
-  <li id='tempval'><span>Tempreture</span> ${data.main.temp}</li>
+  <li id='tempval'><span>Temperature</span> ${data.main.temp}</li>
   <li><span>Weather</span> ${data.weather[0].description} <img src='http://openweathermap.org/img/w/${data.weather[0].icon}.png'></li>
   `;
 };
