b0df58998e107dcfae865a2e43eff3daffb6ab29
ammarkandel <merom8703@gmail.com>
2021-04-15 21:17:05 +0200

    Add change degree function

--- src/app/components/accessData.js
+++ src/app/components/accessData.js
@@ -3,6 +3,16 @@ import moment from 'moment';
 const printCityName = document.querySelector('.city_name');
 const weatherDetails = document.querySelector('.weather_details');
 
+const changeDgree = (data) => {
+  const temp = document.createElement('li');
+  const btnDeg = document.getElementById('convert').textContent.slice(0, 1);
+  let degree;
+
+  btnDeg === 'F' ? degree = 'C' : degree = 'F';
+  temp.innerHTML = `<span>Tempreture</span> ${data.main.temp} °${degree}`;
+  weatherDetails.appendChild(temp);
+}
+
 const printData = (data) => {
   printCityName.textContent = data.name;
   const sunrise = moment(new Date(data.sys.sunrise * 1000)).format('h:mm:ss a');
@@ -13,13 +23,7 @@ const printData = (data) => {
   <li><span>Wind speed</span> ${data.wind.speed} meters/s</li>
   <li><span>Weather</span> ${data.weather[0].description} <img src='http://openweathermap.org/img/w/${data.weather[0].icon}.png'></li>
   `;
-
-  const temp = document.createElement('li');
-  const btnDeg = document.getElementById('convert').textContent.slice(0, 1);
-  let degree = '';
-  btnDeg === 'F' ? degree = 'C' : degree = 'F';
-  temp.innerHTML = `<span>Tempreture</span> ${data.main.temp} °${degree}`;
-  weatherDetails.appendChild(temp);
+  changeDgree(data);
 };
 
 export default printData;
